5a575c1f1c8cd45d7f12cdbd802f61c5e3db6ba7
Kachana, Pujith Reddy <pkachana3@gatech.edu>
2021-10-24 01:32:05 -0400

    First Commit

--- app/src/main/java/com/example/fitmoji/FitMakerPage.kt
+++ app/src/main/java/com/example/fitmoji/FitMakerPage.kt
@@ -1,17 +1,23 @@
 package com.example.fitmoji
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RadioGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
 class FitMakerPage : Fragment(), View.OnClickListener {
     var navController: NavController? = null
+    var shirt = "blackshirt"
+    var pant = "blackpant"
+    var shoe = "blackshoe"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +35,14 @@ class FitMakerPage : Fragment(), View.OnClickListener {
         view.findViewById<Button>(R.id.Vote).setOnClickListener(this)
         view.findViewById<Button>(R.id.BackBtn).setOnClickListener(this)
         view.findViewById<Button>(R.id.ShirtBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.PantBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.ShoeBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.blackShirtBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.redShirtBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.blackPantBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.blackShortBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.shoeBtn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.sandalBtn).setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
@@ -40,8 +54,91 @@ class FitMakerPage : Fragment(), View.OnClickListener {
                 view?.findViewById<RadioGroup>(R.id.ShirtsGroup)!!.visibility = View.GONE
                 view?.findViewById<RadioGroup>(R.id.PantsGroup)!!.visibility = View.GONE
                 view?.findViewById<RadioGroup>(R.id.ShoesGroup)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.ShirtBtn)!!.visibility = View.VISIBLE
+                view?.findViewById<Button>(R.id.PantBtn)!!.visibility = View.VISIBLE
+                view?.findViewById<Button>(R.id.ShoeBtn)!!.visibility = View.VISIBLE
             }
-            R.id.ShirtBtn -> view?.findViewById<RadioGroup>(R.id.ShirtsGroup)!!.visibility = View.VISIBLE
+            R.id.ShirtBtn -> {
+                view?.findViewById<RadioGroup>(R.id.ShirtsGroup)!!.visibility = View.VISIBLE
+                view?.findViewById<Button>(R.id.ShirtBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.PantBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.ShoeBtn)!!.visibility = View.GONE
+            }
+            R.id.PantBtn -> {
+                view?.findViewById<RadioGroup>(R.id.PantsGroup)!!.visibility = View.VISIBLE
+                view?.findViewById<Button>(R.id.ShirtBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.PantBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.ShoeBtn)!!.visibility = View.GONE
+            }
+            R.id.ShoeBtn -> {
+                view?.findViewById<RadioGroup>(R.id.ShoesGroup)!!.visibility = View.VISIBLE
+                view?.findViewById<Button>(R.id.ShirtBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.PantBtn)!!.visibility = View.GONE
+                view?.findViewById<Button>(R.id.ShoeBtn)!!.visibility = View.GONE
+            }
+            R.id.blackShirtBtn -> {
+                shirt = "blackshirt"
+                updateOutfit()
+            }
+            R.id.redShirtBtn -> {
+                shirt = "redshirt"
+                updateOutfit()
+            }
+            R.id.blackPantBtn -> {
+                pant = "blackpant"
+                updateOutfit()
+            }
+            R.id.blackShortBtn -> {
+                pant = "blackshort"
+                updateOutfit()
+            }
+            R.id.shoeBtn -> {
+                shoe = "blackshoe"
+                updateOutfit()
+            }
+            R.id.sandalBtn -> {
+                shoe = "sandal"
+                updateOutfit()
+            }
         }
+    }
+
+    private fun updateOutfit() {
+        if (shirt.equals("blackshirt")) {
+            if (pant.equals("blackpant")) {
+                if (shoe.equals("blackshoe")) {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_blackshirt_blackpant_blackshoe)
+                }
+                else {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_blackshirt_blackpant_blackshoe)
+                }
+            } else {
+                if (shoe.equals("blackshoe")) {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_blackshirt_blackshort_blackshoe)
+                }
+                else {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_blackshirt_blackshort_sandal)
+                }
+            }
+        } else {
+            if (pant.equals("blackpant")) {
+                if (shoe.equals("blackshoe")) {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_redshirt_blackpant_blackshoe)
+                }
+                else {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_redshirt_blackpant_sandal)
+                }
+            } else {
+                if (shoe.equals("blackshoe")) {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_redshirt_blackshort_blackshoe)
+                }
+                else {
+                    view?.findViewById<ImageView>(R.id.FitMojiImg)?.setImageResource(R.drawable.man_redshirt_blackshort_sandal)
+                }
+            }
+        }
+        var src = "man_"+shirt+"_"+pant+"_"+shoe+".png"
+        var drawableId = this.getResources().getIdentifier(src, "drawable", context?.getPackageName())
+
     }
 }
